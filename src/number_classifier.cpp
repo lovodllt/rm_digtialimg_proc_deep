e801923bc16760debb69c58ccf31c8aa8c5ecf3e
haomogg <385371785@qq.com>
2024-11-03 17:51:23 +0800

    Annotate the code.

--- src/number_classifier.cpp
+++ src/number_classifier.cpp
@@ -40,92 +40,103 @@ static void using_once()
 //    return opened_image;
 //}
 
-void extractNumbers(const cv::Mat & src, std::vector<OneArmor> & armors)
+void extractNumbers(const cv::Mat & src, std::vector<OneArmor> & armors) //用于提取装甲板上的数字
 {
     // static int num = 0;
+    //常量定义
     // Light length in image
-    const int light_length = 12;
+    const int light_length = 12; //装甲板灯条长度
     // Image size after warp
-    const int warp_height = 28;
-    const int small_armor_width = 32;
-    const int large_armor_width = 54;
+    const int warp_height = 28; //透视变换后的图像高度
+    const int small_armor_width = 32; //小装甲板的宽度
+    const int large_armor_width = 54; //大装甲板的宽度
     // Number ROI size
-    const cv::Size roi_size(20, 28);
+    const cv::Size roi_size(20, 28); //数字区域的大小
 
+    //遍历每个装甲板
     for (auto & armor : armors) {
-        // Warp perspective transform
-        cv::Point2f lights_vertices[4] = {armor.objects_keypoints[1], armor.objects_keypoints[0], armor.objects_keypoints[3], armor.objects_keypoints[2]};
+        //透视变换 Warp perspective transform
+        cv::Point2f lights_vertices[4] = {armor.objects_keypoints[1], armor.objects_keypoints[0], armor.objects_keypoints[3], armor.objects_keypoints[2]}; //lights_vertices：装甲板上四个关键点的坐标
 
-        const int top_light_y = (warp_height - light_length) / 2 - 1;
+        const int top_light_y = (warp_height - light_length) / 2 - 1; //计算灯条变换后在图像中的上下边界
         const int bottom_light_y = top_light_y + light_length;
         const int warp_width = small_armor_width;//全按小装甲板处理
-        cv::Point2f target_vertices[4] = {
+        cv::Point2f target_vertices[4] = { //目标图像中四个顶点的坐标
                 cv::Point(0, bottom_light_y),
                 cv::Point(0, top_light_y),
                 cv::Point(warp_width - 1, top_light_y),
                 cv::Point(warp_width - 1, bottom_light_y),
         };
         cv::Mat number_image;
-        auto rotation_matrix = cv::getPerspectiveTransform(lights_vertices, target_vertices);
-        cv::warpPerspective(src, number_image, rotation_matrix, cv::Size(warp_width, warp_height));
+        auto rotation_matrix = cv::getPerspectiveTransform(lights_vertices, target_vertices); //计算透视变换矩阵
+        cv::warpPerspective(src, number_image, rotation_matrix, cv::Size(warp_width, warp_height)); //将源图像应用透视变换矩阵转换为目标图像
 
-        // Get ROI
+        //提取数字区域 Get ROI
         number_image =
                 number_image(cv::Rect(cv::Point((warp_width - roi_size.width) / 2, 0), roi_size));
 
-        // Binarize
+        //二值化处理 Binarize
         std::vector<cv::Mat> channels(3);
-        cv::split(number_image, channels);
+        cv::split(number_image, channels); //将图像拆分为三个通道
 
-        cv::cvtColor(number_image, number_image, cv::COLOR_RGB2GRAY);
+        cv::cvtColor(number_image, number_image, cv::COLOR_RGB2GRAY); //将RGB图像转换为灰度图像
 //保存数字图案20*28
         // cv::imwrite("/home/gx/rm_classifier_training-main/补_/"+std::to_string(num++)+".jpg", number_image);
-        cv::threshold(number_image, number_image, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
+        cv::threshold(number_image, number_image, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU); //使用Otsu方法进行二值化处理
         // cv::Mat number_img_ = perform_opening(number_image,2);
 
+        //存储数字图像
         armor.number_img = number_image;
 
     }
 }
 
-std::vector<OneArmor> classify(cv::Mat src,std::vector<OneArmor> & armors)
+std::vector<OneArmor> classify(cv::Mat src,std::vector<OneArmor> & armors) //对装甲板上的数字进行分类
 {
+    //提取出数字
     extractNumbers(src,armors);
 
+    //初始化模型
     using_once();
     std::vector<OneArmor> armors_data_;
 
+    //遍历每个装甲板
     for (auto & armor : armors) {
+
+        //图像预处理
         cv::Mat image = armor.number_img.clone();
 
         // Normalize
-        image = image / 255.0;
+        image = image / 255.0; //归一化
 
-        // Create blob from image
+        //创建Blob  Create blob from image
         cv::Mat blob;
-        cv::dnn::blobFromImage(image, blob);
+        cv::dnn::blobFromImage(image, blob); //将图像转换为blob格式
 
-        // Set the input blob for the neural network
-        net_.setInput(blob);
+        //设置输入并前向传播 Set the input blob for the neural network
+        net_.setInput(blob); //将 Blob 设置为神经网络的输入
         // Forward pass the image blob through the model
-        cv::Mat outputs = net_.forward();
+        cv::Mat outputs = net_.forward(); //前向传播图像，得到模型的输出
 
-        // Do softmax
-        float max_prob = *std::max_element(outputs.begin<float>(), outputs.end<float>());
+        //Softmax处理
+        float max_prob = *std::max_element(outputs.begin<float>(), outputs.end<float>()); //找到输出中的最大值max_prob
         cv::Mat softmax_prob;
-        cv::exp(outputs - max_prob, softmax_prob);
-        float sum = static_cast<float>(cv::sum(softmax_prob)[0]);
+        cv::exp(outputs - max_prob, softmax_prob); //计算 Softmax 概率
+        float sum = static_cast<float>(cv::sum(softmax_prob)[0]); //计算概率和，并将概率归一化
         softmax_prob /= sum;
 
+        //获取分类结果
         double confidence;
         cv::Point class_id_point;
-        minMaxLoc(softmax_prob.reshape(1, 1), nullptr, &confidence, nullptr, &class_id_point);
-        int label_id = class_id_point.x;
+        minMaxLoc(softmax_prob.reshape(1, 1), nullptr, &confidence, nullptr, &class_id_point); //minMaxLoc函数找到最大概率的位置class_id_point
+        int label_id = class_id_point.x; //label_id:分类结果的标签索引
 
+        //更新装甲板信息
         armor.number = class_names_[label_id];
 
+        //过滤负样本
         for(auto i:armors){
-            if(i.number != "negative"){
+            if(i.number != "negative"){ //滤掉number为"negative"的装甲板
                 armors_data_.push_back(i);
             }
         }
--- src/video_detect_test.cpp
+++ src/video_detect_test.cpp
@@ -3,23 +3,29 @@
 #include "../include/number_classifier.hpp"
 #define VIDEO_PATH "../video_test/test.mp4"
 
-void show_points_result(cv::Mat& img,std::vector<OneArmor> armors_data ) {
+void show_points_result(cv::Mat& img,std::vector<OneArmor> armors_data ) { //展示装甲板的关键点
+
+    //定义颜色映射
     std::map<int , std::string>colors ;
     colors[1] = "red";
     colors[0] = "blue";
+
+    //遍历每个装甲板
     for (auto i: armors_data) {
        // cv::rectangle(img, i.box, cv::Scalar(0, 255, 0), 2);
-       for(int j=0;j<4;j++){
+       for(int j=0;j<4;j++){ //绘制装甲板轮廓
            cv::line(img, i.objects_keypoints[j], i.objects_keypoints[(j + 1) % 4], cv::Scalar(0, 255, 0), 1);
        }
-       for(int j=0;j<4;j++){
+       for(int j=0;j<4;j++){ //绘制关键点
            if(j == 0){
-               cv::circle(img, i.objects_keypoints[j], 2, cv::Scalar(0, 0, 255), -1);
-           }else if(j==1){     cv::circle(img, i.objects_keypoints[j], 2, cv::Scalar(0, 0, 0), -1);}
-           else if(j==2){     cv::circle(img, i.objects_keypoints[j], 2, cv::Scalar(0, 255, 0), -1);}
-           else if(j==3){     cv::circle(img, i.objects_keypoints[j], 2, cv::Scalar(255, 0, 0), -1);}
+               cv::circle(img, i.objects_keypoints[j], 2, cv::Scalar(0, 0, 255), -1); //第一个关键点(j==0)用红色(cv::Scalar(0,0,255)
+           }else if(j==1){     cv::circle(img, i.objects_keypoints[j], 2, cv::Scalar(0, 0, 0), -1);} //第二个关键点(j==1)用黑色(cv::Scalar(0,0,0))
+           else if(j==2){     cv::circle(img, i.objects_keypoints[j], 2, cv::Scalar(0, 255, 0), -1);} //第三个关键点(j==2)用绿色(cv::Scalar(0,255,0))
+           else if(j==3){     cv::circle(img, i.objects_keypoints[j], 2, cv::Scalar(255, 0, 0), -1);} //第四个关键点(j==3)用蓝色(cv::Scalar(255,0,0))
        }
        //cv::rectangle(img, i.box, cv::Scalar(0, 255, 0), 2);
+
+       //绘制类别标签
        cv::putText(img, colors[i.class_ids], i.objects_keypoints[0], cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(0, 255, 0), 2);
     }
 }
@@ -28,7 +34,7 @@ void show_box_result(cv::Mat& img,std::vector<OneArmor> armors_data ) {
         cv::rectangle(img, i.box, cv::Scalar(0, 255, 0), 2);
     }
 }
-void show_number_result(cv::Mat& img,std::vector<OneArmor> armors_data){
+void show_number_result(cv::Mat& img,std::vector<OneArmor> armors_data){ //展示识别出来的数字
     for(auto i: armors_data){
         cv::putText(img, i.number, i.objects_keypoints[3], cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(0, 255, 0), 2);
     }
@@ -36,14 +42,16 @@ void show_number_result(cv::Mat& img,std::vector<OneArmor> armors_data){
 int main(int argc, char* argv[])
 {
 
+    //视频捕获初始化
+    cv::VideoCapture capture(VIDEO_PATH); // VideoCapture:用于从视频文件、摄像头或其他视频流中捕获视频
 
-    cv::VideoCapture capture(VIDEO_PATH);
-
+    //检查视频是否成功打开
     if (!capture.isOpened())
     {
-        std::cout << "无法读取视频：" << argv[1] << std::endl;
+        std::cout << "无法读取视频：" << argv[1] << std::endl; //argv[1]:显示尝试打开的视频文件名
         return -1;
     }
+
     // 读取视频帧，使用Mat类型的frame存储返回的帧
     cv::Mat frame;
 
@@ -51,30 +59,34 @@ int main(int argc, char* argv[])
     while (true){
 
 
-        capture >> frame;
+        capture >> frame; //从视频流中读取下一帧图像(capture中存储视频，一帧一帧写入frame中)
         if (frame.empty())
         {
             std::cout << "视频读取完毕" << std::endl;
             break;
         }
-        cv::TickMeter tm;
-        tm.start();
-        dataImg blob = preprocessImage(frame);
+        //时间测量
+        cv::TickMeter tm; //TickMeter:测量代码块的执行时间。它通过记录 CPU 的时钟周期来实现高精度的时间测量
+        tm.start(); //开始计时
+
+        //图像预处理与推理
+        dataImg blob = preprocessImage(frame); //对输入的图像帧进行预处理
       //  std::cout<<"start infer"<<std::endl;
-        auto armors_data = startInferAndNMS(blob);
+        auto armors_data = startInferAndNMS(blob); //深度推理
         //传统处理获得角点以提高角点精度（实测非必须）
         //auto armors_data_ = tradition(frame, armors_data);
-        //获得数字
-        auto armors_data_ =  classify(frame,armors_data);
-        tm.stop();
-        std::cout << "time cost: " << tm.getTimeMilli() << "ms" << std::endl;
-        show_number_result(frame, armors_data_);
-        show_points_result(frame, armors_data_);
+
+        //后处理与结果展示
+        auto armors_data_ =  classify(frame,armors_data); //获得数字并进行数字分类
+        tm.stop(); //停止计时
+        std::cout << "time cost: " << tm.getTimeMilli() << "ms" << std::endl; //getTimeMilli():获取并输出整个处理过程的时间消耗（以毫秒为单位）
+        show_number_result(frame, armors_data_); //绘制数字
+        show_points_result(frame, armors_data_); //绘制装甲板的框和关键点
        // show_box_result(frame, armors_data_);
 
-        // 按下ESC键退出
+        //用户交互与窗口管理
         int k = cv::waitKey(10);
-        if (k == 27)
+        if (k == 27) // 按下ESC键退出(ASCII码为27)
         {
             std:: cout << "退出" << std::endl;
             break;
