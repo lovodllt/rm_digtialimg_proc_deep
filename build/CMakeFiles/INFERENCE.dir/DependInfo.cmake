4750174b91b7cdd4181a3e729dde0a9d9ee915b0
haomogg <385371785@qq.com>
2024-11-03 10:26:42 +0800

    git init.

---
+++ build/CMakeFiles/INFERENCE.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/haomo/catkin_ws/src/Reborn-Vision-2024-armor-Inference/src/inference.cpp" "/home/haomo/catkin_ws/src/Reborn-Vision-2024-armor-Inference/build/CMakeFiles/INFERENCE.dir/src/inference.cpp.o"
+  "/home/haomo/catkin_ws/src/Reborn-Vision-2024-armor-Inference/src/number_classifier.cpp" "/home/haomo/catkin_ws/src/Reborn-Vision-2024-armor-Inference/build/CMakeFiles/INFERENCE.dir/src/number_classifier.cpp.o"
+  "/home/haomo/catkin_ws/src/Reborn-Vision-2024-armor-Inference/src/tradition.cpp" "/home/haomo/catkin_ws/src/Reborn-Vision-2024-armor-Inference/build/CMakeFiles/INFERENCE.dir/src/tradition.cpp.o"
+  "/home/haomo/catkin_ws/src/Reborn-Vision-2024-armor-Inference/src/video_detect_test.cpp" "/home/haomo/catkin_ws/src/Reborn-Vision-2024-armor-Inference/build/CMakeFiles/INFERENCE.dir/src/video_detect_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/intel/openvino_2023.3.0/runtime/include/ie"
+  "/opt/intel/openvino_2023/runtime/include/ngraph"
+  "/opt/intel/openvino_2023.3.0/runtime/include/openvino"
+  "/opt/intel/openvino_2023.3.0/runtime/include"
+  "../include"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/INFERENCE.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/INFERENCE.dir/src/inference.cpp.o"
+  "CMakeFiles/INFERENCE.dir/src/number_classifier.cpp.o"
+  "CMakeFiles/INFERENCE.dir/src/tradition.cpp.o"
+  "CMakeFiles/INFERENCE.dir/src/video_detect_test.cpp.o"
+  "INFERENCE"
+  "INFERENCE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/INFERENCE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
