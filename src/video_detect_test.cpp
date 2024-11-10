0f891c908627e05a9b01b3e20a947f2cb0ad8799
haomogg <385371785@qq.com>
2024-11-10 17:10:12 +0800

    Encapsulate into ros successfully.

--- src/video_detect_test.cpp
+++ src/video_detect_test.cpp
@@ -1,3 +1,4 @@
+/*
 #include "../include/inference.hpp"
 #include "../include/tradition.hpp"
 #include "../include/number_classifier.hpp"
@@ -54,4 +55,51 @@ int main(int argc, char* argv[])
 
     }
     return 0;
+}
+*/
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/Image.h>
+#include "../include/inference.hpp"
+#include "../include/tradition.hpp"
+#include "../include/number_classifier.hpp"
+
+// 定义回调函数处理接收到的图像
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+  try
+  {
+    cv::Mat frame = cv_bridge::toCvShare(msg, "bgr8")->image;
+
+    cv::TickMeter tm;
+    tm.start();
+    dataImg blob = preprocessImage(frame);
+    auto armors_data = startInferAndNMS(blob);
+    auto armors_data_ = classify(frame, armors_data);
+    tm.stop();
+    std::cout << "time cost: " << tm.getTimeMilli() << "ms" << std::endl;
+    show_number_result(frame, armors_data_);
+    show_points_result(frame, armors_data_);
+
+    // 显示结果
+    cv::imshow("result", frame);
+    cv::waitKey(1);  // 必须有等待按键，否则窗口会卡住
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+  }
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "video_detector_node");
+  ros::NodeHandle nh;
+  image_transport::ImageTransport it(nh);
+  image_transport::Subscriber sub = it.subscribe("camera/image_raw", 1, imageCallback);
+
+  ros::spin();
+  return 0;
 }
